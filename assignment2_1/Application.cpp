88cfa789e3491d4fedb792c34697090a9c5e6458
olegsfinest <paul.wille@t-online.de>
2014-04-30 13:01:33 +0200

    final version assignment 2

--- assignment2_1/Application.cpp
+++ assignment2_1/Application.cpp
@@ -10,6 +10,7 @@
 
 #include "DepthCamera.h"
 #include "DepthCameraException.h"
+#include <iostream>
 
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -39,28 +40,76 @@ void Application::processFrame()
 	
 
 	using namespace cv;
-	
+	using namespace std;
+
 	//create Baseframe
 	if (firstRun) {
 		m_depthImage.copyTo(m_baseFrame);
 		firstRun = false;
 	}
 	absdiff(m_depthImage, m_baseFrame, temp);
-	// Because the depth cam does not create an exact image, we get rid of the small noises bz eroding it
-	erode(temp, temp,getStructuringElement(MORPH_RECT, Size(12,12))); 
-	// To gain the original size we dilate it afterwards
-	dilate(temp,temp,getStructuringElement(MORPH_RECT, Size(12,12)));
+
+	//convert to 8bit 
 	temp.convertTo(temp, CV_8UC1);
+	
+	//cutoff everything closer to the camera than about 10cm above the baseimage
 	threshold(temp, temp, 60, 255, THRESH_TOZERO_INV);
+	//cutoff everything too far away
 	threshold(temp, temp, 20, 255, 0);
-	//threshold(temp, temp, 60, 0, 0);								// | CV_THRESH_OTSU
 
 	//cvtColor(m_rgbImage,temp , CV_RGB2GRAY);
 	//threshold(temp, temp, 100, 255, CV_THRESH_BINARY);
 
+	// Because the depth cam does not create an exact image, we get rid of the small noises bz eroding it
+	erode(temp, temp,getStructuringElement(MORPH_RECT, Size(12,12))); 
+
+	// To gain the original size we dilate it afterwards
+	dilate(temp,temp,getStructuringElement(MORPH_RECT, Size(12,12)));
+
+	vector<vector<Point> > contours;
+	double maxContourArea = 0;
+	int maxContourIndex = 0;
+	int i;
+	double contourArea;
+	vector<Point> maxContour;
+	RotatedRect fittedBox;
+
+	//find all contours in Image and save to contours-variable
+	findContours(temp, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+	//iterate over all contours and find the biggest
+	for(i = 0; i < (int)contours.size(); i++){
+		//smaller contours will crash the programm
+		if (contours[i].size() < 6) continue;
+		
+		contourArea = cv::contourArea(contours[i]);
+	
+		if (contourArea > maxContourArea) {
+			maxContourIndex = i;
+			maxContourArea = contourArea;
+			maxContour = contours[i];
+		}
+	}
+	//cout << "MaxContourArea: "<< maxContourArea << '\n';
+	//cout << "MaxContourSize: " << (int)contours.size() << '\n';
+	//cout << "MaxContourIndex: " << maxContourIndex << '\n';
+	
+
+	//CHECK ONCE AGAIN BECAUSE IT DOES NOT MAKE SENSE BUT IF WE DO NOT THE WHOLE PROGRAMM CRASHES WITHOUT A DEBUGGING NOTICE, THIS TOOK US AT LEAST 4 HOURS
+	//!!!!!!!!!!!!!!!!!!!!!!!!11!!!!!!!!!!!!!!!!111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11!!EINSELF!!!!!
+	if (maxContour.size() > 5) {
+		fittedBox = fitEllipse(maxContour);
+		//draw circle around center of fitted Ellipse
+		circle(temp, fittedBox.center, 35, Scalar(100, 150, 200, 0), 2);
+	}	
 	temp *=32;
 	temp.copyTo(m_outputImage);
 
+
+	//vector<Point> maxContour = contours.at(maxContourIndex);
+	//Mat convertedContour;
+	//Mat(contours[maxContourIndex]).convertTo(convertedContour, CV_32F);
+	//fittedBox = fitEllipse(convertedContour);
 
 }
 
